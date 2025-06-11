fb6e88027493283d757da6f06ec78e5225dc175c
Antoine Lavandier <antoine.lavandier@inria.fr>
2025-06-11 15:28:28 +0200

    Wasmi Example

---
+++ examples/wasmi/README.md
@@ -0,0 +1,47 @@
+# hello-world-async
+
+## About
+
+This application is a test of the possibility to run the wasmi interpreter on Board supported by Ariel
+The `wasm` is present in textual form in [`ex.wat`](ex.wat) and in binary form in `low_mem_example.wasm`.
+They were compiled from single `lib.rs` containing :
+```rust
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+extern "C" fn add(left: u32, right: u32) -> u32 {
+    unsafe { host_hello(32); };
+    left + right
+}
+
+#[link(wasm_import_module = "host")]
+unsafe extern "C" {
+    fn host_hello(i: u32);
+}
+
+
+#[panic_handler]
+fn stall_on_panic(_msg: &PanicInfo) -> ! {
+    loop { }
+}
+```
+by following these [instructions](https://surma.dev/things/rust-to-webassembly/). It boils down to declaring a lib crate with
+```toml
+[lib]
+crate-type = ["cdylib", "rlib"]
+```
+and building it with
+```bash
+cargo rustc --target=wasm32-unknown-unknown --release -- -Clink-arg=-zstack-size=16 -Clink-arg=--initial-memory=65536
+```
+The stack size needs to be changed because by default it is set to `1MB` and thus requires the `wasm` programm to also ask for `1MB` of inital memory which is more than the board has.
+The initial and max memory need to be multiples of 65536 bytes by default. There is some support in the `wasm` specs (and in `wasmi`) for non standard page size but this hasn't been tested yet.
+This has only been tested on the `nrf52840fk`. `wasmi` needs an allocator and atomics to compile.
+
+## How to run
+
+In this directory, run
+
+    laze build -b nrf52840dk run
---
+++ examples/wasmi/src/main.rs
@@ -0,0 +1,70 @@
+#![no_main]
+#![no_std]
+
+use ariel_os::debug::{exit, log::{defmt, *}, ExitCode};
+
+use wasmi::*;
+
+#[ariel_os::task(autostart)]
+async fn main() {
+    info!("Testing Wasmi on this board");
+    let wasm_r = run_wasm();
+    match wasm_r {
+        Ok(v)  => {
+            info!("Got {} by running wasm", v);
+        },
+        Err(e) => {
+            info!("{}", defmt::Display2Format(&e));
+        }
+    }
+    exit(ExitCode::SUCCESS);
+}
+
+fn run_wasm() -> Result<u32, wasmi::Error> {
+
+    let wasm  = include_bytes!("../low_mem_example.wasm");
+    // First step is to create the Wasm execution engine with some config.
+    // In this example we are using the default configuration.
+    let engine = Engine::default();
+    // Now we can compile the above Wasm module with the given Wasm source.
+    let module = Module::new(&engine, wasm)?;
+
+
+    //
+    for imp in module.imports() {
+        info!("{:?}", defmt::Debug2Format(&imp));
+    }
+    for exp in module.exports() {
+        info!("{:?}", defmt::Debug2Format(&exp));
+    }
+
+    // Wasm objects operate within the context of a Wasm `Store`.
+    //
+    // Each `Store` has a type parameter to store host specific data.
+    // In this example the host state is a simple `u32` type with value `42`.
+    type HostState = u32;
+    let mut store = Store::new(&engine, 42);
+
+    info!("Created Store");
+    // A linker can be used to instantiate Wasm modules.
+    // The job of a linker is to satisfy the Wasm module's imports.
+
+    let mut linker = <Linker<HostState>>::new(&engine);
+    info!("Created Linker");
+    // We are required to define all imports before instantiating a Wasm module.
+    let _ = linker.func_wrap("host", "host_hello", |caller: Caller<'_, HostState>, param: u32| {
+        info!("Got {} from WebAssembly and my host state is: {}",param, caller.data());
+    })?;
+
+    info!("Linking Done");
+    let instance = linker.instantiate(&mut store, &module)?
+        .start(&mut store)?;
+
+    info!("Instantiating done");
+    // Now we can finally query the exported "hello" function and call it.
+    let res = instance
+        .get_typed_func::<(u32, u32), u32>(&store, "add")?
+        .call(&mut store, (2, 2))?;
+    Ok(res)
+    // Ok(0)
+}
